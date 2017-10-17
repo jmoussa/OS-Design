21c72fc92ce24f6ccd3819be26c377d17fbf5676
nandanthakkar <nandanthakkar00@gmail.com>
2017-10-17 14:44:15 -0400

    Update my_pthread_t.h

--- my_pthread_t.h
+++ my_pthread_t.h
@@ -4,7 +4,7 @@
 
 // name:
 // username of iLab:
-// iLab Server: 
+// iLab Server:
 #ifndef MY_PTHREAD_T_H
 #define MY_PTHREAD_T_H
 
@@ -15,7 +15,7 @@
 #define pthread_join(a, b) my_pthread_join(a, b)
 #define my_pthread_exit(x) my_my_pthread_exit(x)
 //macro my_ to mutex functions
-#define pthread_mutex_init(a, b)  my_pthread_mutex_init(a, b) 
+#define pthread_mutex_init(a, b)  my_pthread_mutex_init(a, b)
 #define pthread_mutex_lock(a) my_pthread_mutex_lock(a)
 #define pthread_mutex_unlock(a) my_pthread_mutex_unlock(a)
 #define pthread_mutex_destroy(a)	my_pthread_mutex_destroy(a)
@@ -30,6 +30,24 @@
 #include <ucontext.h>
 #include <errno.h>
 
+
+//StackSize of each thread : 16 Kilobytes
+#define MEM 16384
+#define LEVELS 4
+//Quantum duration : 25 ms
+#define QUANTUM 25000
+//Check for starvation every 50 quanta
+#define MAINTAINENCE_CYCLE_THRESHOLD 50
+
+int maintainence_cycle_counter = 0;
+
+//Thread states
+typedef enum state
+{
+    READY, RUNNING, WAITING, YIELDED, EXITED
+} state;
+
+
 typedef uint my_pthread_t;
 int tcb_num=0;
 
@@ -39,57 +57,69 @@ struct thread_info {
     void *arg;//arguments for function
     int joinable;
     int queue;
-    int readyTime;
+    long int readyTime;
     int execTime;
-    int deadline;
+    long int deadline;
     struct thread_info *recov_info;
 };
 
 
 //Thread Control Block
-typedef struct tcb {
-	/* add something here */
+typedef struct threadControlBlock {
+    /* add something here */
     my_pthread_t tid;
     my_pthread_t ptid;
-    void * result;
+    void * retval;
     void ** join_retval;
-    int status;//0=joined 1=yielded 2=exited 3=running
+    state status; // Created enum state ; would not need this. 0=joined 1=yielded 2=exited 3=running
     ucontext_t thread_context;//stores the context of the thread
     struct thread_info thread_params;//thread info
     uint executedTime;//tells how long program has been running
-    struct tcb *childThread;
+    struct threadControlBlock * nextThread;
     uint priority;//used by scheduler object
     uint magic_key;//used for debugging
-} ; 
+}tcb;
 
 // MUTEX Struct Definition
 typedef struct my_pthread_mutex_t {
-	int lock; //0 or 1, set to 0(unlocked) by default
+    int lock; //0 or 1, set to 0(unlocked) by default
     int flags; //TODO: Define further?
 } my_pthread_mutex_t;
 
-struct Node{
-    struct tcb  head;
-    struct Node * next;
+struct queue{
+    tcb * front;
+    tcb * back;
+    int counter;
 };
 
-struct Node Queue[7]; // all queues 4=running,5=waiting,6=completed
-struct tcb tcbs[1000]; // all the tcbs that you could have are intialized.
-struct tcb *tcbPtr;
+
+struct queue Queue[7]; // all queues 5=running,6=waiting,7=completed
+
+
+tcb *tcbs[1000]; // all the tcbs that you could have are intialized.
+
 //pointers to back of each queue
 struct Node* back[7];
+
 //pointers to the front of each queue
 struct Node* front[7];
+
 sigset_t sigProcMask;
 // Feel free to add your own auxiliary data structures
 
-struct itimerval it; 
+struct itimerval it;
 struct sigaction act, oact;
 
+//next thread to run for the scheduler
+tcb * get_next_thread_to_run();
+//get current time
+long int get_current_time();
 //adds tcb to queue
-int enqueue(struct tcb my_tcb,int a);
+//int enqueue(tcb* my_tcb,int a);
+int my_enqueue(struct queue * my_queue, tcb * my_tcb);
 //removes tcb to from queue
-int dequeue(int a);
+//int dequeue(int a);
+tcb * my_dequeue(struct queue * my_queue);
 //looks at next tcb in priority queue
 struct Node* peek();
 /* Function Declarations: */
