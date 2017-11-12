b5bee13119eacd8c9eeafbd1ccd00299cf939ca5
aniltilve <tilveanil@gmail.com>
2017-11-12 17:19:43 -0500

    Added PAGE_SIZE define

--- my_alloc.h
+++ my_alloc.h
@@ -3,6 +3,7 @@
 
 #define PHYSICAL_MEM_SIZE 8388608 //8 MB
 #define SWAP_FILE_SIZE 16777216 //16 MB
+#define PAGE_SIZE sysconf(_SC_PAGE_SIZE)
 
 #define malloc(x) myallocate(x, __FILE__, __LINE__, THREADREQ)
 #define free(x) mydeallocate(x, __FILE__, __LINE__, THREADREQ)
