e7b380305f844879b8316d640aac5f2fe4934d92
aniltilve <tilveanil@gmail.com>
2017-11-12 17:04:53 -0500

    Added size defines

--- my_alloc.h
+++ my_alloc.h
@@ -1,6 +1,9 @@
 #ifndef MY_ALLOCATE_H
 #define MY_ALLOCATE_H
 
+#define PHYSICAL_MEM_SIZE 8388608 //8 MB
+#define SWAP_FILE_SIZE 16777216 //16 MB
+
 #define malloc(x) myallocate(x, __FILE__, __LINE__, THREADREQ)
 #define free(x) mydeallocate(x, __FILE__, __LINE__, THREADREQ)
 
