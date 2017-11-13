ca2dff6acdd25ddd65485e6af89ba5b6d883324e
aniltilve <tilveanil@gmail.com>
2017-11-13 18:55:47 -0500

    Added stuff

--- my_alloc.h
+++ my_alloc.h
@@ -7,5 +7,17 @@
 
 #define malloc(x) myallocate(x, __FILE__, __LINE__, THREADREQ)
 #define free(x) mydeallocate(x, __FILE__, __LINE__, THREADREQ)
+#define LIBRARYREQ 0
+#define THREADREQ 1
+#define DISKREQ 2
+
+typedef struct Page_t {
+    int isFree;
+} Page;
+
+typedef struct Segment_t {
+    int test;
+    struct Segment_t* next;
+} Segment;
 
 #endif
